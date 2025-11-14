0d88af83744e4b04a097e2d635aa0223c7d5aad6
Edwin Karanja <edwin.karanja@m-kopa.com>
2025-11-14 11:28:23 +0300

    Deleted version one

--- config/__init__.py
+++
@@ -1,8 +0,0 @@
-"""M-KOPA Fraud Investigation - Configuration Package"""
-from .settings import *
-from .queries import SQL_QUERIES
-from .instructions import (
-    get_query_planning_prompt, 
-    get_investigation_prompt, 
-    TRAINING_EXAMPLES
-)
